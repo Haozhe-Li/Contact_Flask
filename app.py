d74a0dd552104a2562314de9cb10f964b7a0bfda
Haozhe Li <hl83@illinois.edu>
2024-06-06 14:38:56 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify
+from flask_mail import Mail, Message
+import os
+
+app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = str(os.getenv('MAIL_SERVER'))
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = str(os.getenv('MAIL_USERNAME'))
+app.config['MAIL_PASSWORD'] = str(os.getenv('MAIL_PASSWORD'))
+
+SENDER = str(os.getenv('MAIL_SENDER'))
+FORWARD = str(os.getenv('MAIL_FORWARD'))
+NAME = str(os.getenv('MAIL_NAME'))
+
+mail = Mail(app)
+
+@app.route('/send_email', methods=['POST'])
+def send_email():
+    data = request.json
+    name = data.get('name')
+    email = data.get('email')
+    message = data.get('message')
+    to_myself_msg = Message('New Contact Form Submission',
+                  sender=SENDER,
+                  recipients=[FORWARD])
+    to_myself_msg.body = f"""
+    From: {name} <{email}>
+    Message:
+    {message}
+    """
+    to_client_msg = Message('[{NAME}] THANKS!',
+                  sender=SENDER,
+                  recipients=[email])
+    to_client_msg.body = f"[Auto-Reply] Thank you for contacting us! I will get back to you soon. \n\n Sincerely, \n {NAME}"
+    try:
+        mail.send(to_client_msg)
+    except Exception as e:
+        return jsonify({'error': str(e), 'message': 'send to client failed'}), 500
+    try:
+        mail.send(to_myself_msg)
+        return jsonify({'message': 'Email sent!'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e), 'message':'forward to myself failed'}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
